99abf547e29d1e588897c956324921dc7ec89a34
ashraful2871 <ashrafulislam2758@gmail.com>
2025-08-04 16:36:57 +0600

    implement navbar and footer

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,14 +1,15 @@
 import type { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
 interface IProps {
   children: ReactNode;
 }
 const CommonLayout = ({ children }: IProps) => {
   return (
-    <div>
+    <div className="min-h-screen bg-red-300 flex flex-col ">
       <Navbar></Navbar>
-      {children}
+      <div className="grow-1 container mx-auto">{children}</div>
       <Footer></Footer>
     </div>
   );
