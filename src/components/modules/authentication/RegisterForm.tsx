f0b7f536632abb591ca5f3f641949124f2595876
ashraful2871 <ashrafulislam2758@gmail.com>
2025-08-07 15:51:48 +0600

    added form input

---
+++ src/components/modules/authentication/RegisterForm.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+
+const RegisterForm = ({
+  className,
+  ...props
+}): React.HTMLAttributes<HTMLAnchorElement> => {
+  const form = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Register your account</h1>
+        <p className="text-sm text-muted-foreground">
+          Enter your details to create an account
+        </p>
+      </div>
+
+      <div className="grid gap-6">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+
+        <Button
+          type="button"
+          variant="outline"
+          className="w-full cursor-pointer"
+        >
+          Login with Google
+        </Button>
+      </div>
+
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <Link to="/login" className="underline underline-offset-4">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
